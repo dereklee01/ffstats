a0699757873613620442bc8d4643655f17e33d74
dereklee01 <derek00111@gmail.com>
2022-06-24 00:47:06 -0400

    improved code efficiency and added rb/te

--- config.py
+++ config.py
@@ -11,21 +11,24 @@ def config():
     # scoring = input("Standard or PPR scoring?")
     qb = input("How many quarterback slots?\n")
     rb = input("How many runningback slots?\n")
-    # wr = input("How many wide receiver slots?\n")
+    wr = input("How many wide receiver slots?\n")
+    te = input("How many tight end slots?\n")
     # flex = input("How many flex (WR/RB/TE) slots?\n")
     # k = input("How many kicker slots?")
     # defense = input("How many team defense slots?")
 
     # concatenating and grabbing url string
     url = 'https://www.footballguys.com/playerhistoricalstats?pos=qb&yr=' + year + '&startwk=' + week + '&stopwk=' + week + '&profile=p'
+    rburl = 'https://www.footballguys.com/playerhistoricalstats?pos=rb&yr=' + year + '&startwk=' + week + '&stopwk=' + week + '&profile=p'
     wrurl = 'https://www.footballguys.com/playerhistoricalstats?pos=wr&yr=' + year + '&startwk=' + week + '&stopwk=' + week + '&profile=p'
+    teurl = 'https://www.footballguys.com/playerhistoricalstats?pos=te&yr=' + year + '&startwk=' + week + '&stopwk=' + week + '&profile=p'
 
     qbPts = 0
-    wrPts = 0
     rbPts = 0
+    wrPts = 0
     tePts = 0
     flexPts = 0
     kPts = 0
     defPts = 0
 
-    return [year, week, qb, rb], [url, wrurl], [qbPts, wrPts, rbPts, tePts, flexPts, kPts, defPts]
+    return [year, week, qb, rb, wr, te], [url, rburl, wrurl, teurl], [qbPts, rbPts, wrPts, tePts, flexPts, kPts, defPts]
--- main.py
+++ main.py
@@ -43,11 +43,13 @@ for url in urlList:
             length = len(df)
             df.loc[length] = row
 
-        ptsList[0] = float(df.iloc[0,len(df.loc[0])-1])
+        for x in range(int(float(configList[2]))):
+            ptsList[0] += float(df.iloc[x,len(df.loc[0])-1])
+           
+            # grabbing the highest point output
+            print("QB" + str(x+1) + ": " + df.iloc[x,1] + " " + df.iloc[x,len(df.loc[0])-1] + " points")
 
-        # grabbing the highest point output
-        print("QB: " + df.iloc[0,1] + " " + df.iloc[0,len(df.loc[0])-1] + " points")
-    else:
+    elif "rb" in url:
         # Create a for loop to fill mydata
         # all rows are located under the <tr> tags
         # all row items are located under the <td> tags
@@ -57,11 +59,44 @@ for url in urlList:
             length = len(df)
             df.loc[length] = row
 
-        ptsList[1] = float(df.iloc[0,len(df.loc[0])-1]) + float(df.iloc[1,len(df.loc[0])-1])
+        for x in range(int(float(configList[3]))):
+            ptsList[1] += float(df.iloc[x,len(df.loc[0])-1])
+           
+            # grabbing the highest point output
+            print("RB" + str(x+1) + ": " + df.iloc[x,1] + " " + df.iloc[x,len(df.loc[0])-1] + " points")
 
-        # grabbing the highest point output
-        print("WR1: " + df.iloc[0,1] + " " + df.iloc[0,len(df.loc[0])-1] + " points")
-        print("WR2: " + df.iloc[1,1] + " " + df.iloc[1,len(df.loc[0])-1] + " points")
+    elif "wr" in url:
+        # Create a for loop to fill mydata
+        # all rows are located under the <tr> tags
+        # all row items are located under the <td> tags
+        for j in table1.find_all("tr", limit=int(configList[4])+1)[1:]:
+            row_data = j.find_all("td")
+            row = [i.text for i in row_data]
+            length = len(df)
+            df.loc[length] = row
+
+        for x in range(int(float(configList[4]))):
+            ptsList[2] += float(df.iloc[x,len(df.loc[0])-1])
+           
+            # grabbing the highest point output
+            print("WR" + str(x+1) + ": " + df.iloc[x,1] + " " + df.iloc[x,len(df.loc[0])-1] + " points")
+
+    else:
+        # Create a for loop to fill mydata
+        # all rows are located under the <tr> tags
+        # all row items are located under the <td> tags
+        for j in table1.find_all("tr", limit=int(configList[5])+1)[1:]:
+            row_data = j.find_all("td")
+            row = [i.text for i in row_data]
+            length = len(df)
+            df.loc[length] = row
+
+        for x in range(int(float(configList[5]))):
+            ptsList[3] += float(df.iloc[x,len(df.loc[0])-1])
+           
+            # grabbing the highest point output
+            print("TE" + str(x+1) + ": " + df.iloc[x,1] + " " + df.iloc[x,len(df.loc[0])-1] + " points")
+
 
 print(ptsList)
 total = 0
@@ -69,4 +104,4 @@ for pts in ptsList:
     total += pts
 
 print("\n")
-print("Total points for the " + configList[0] + " season, Week " + configList[1] + ": " + str(total) + " points!")
+print("Max total points for the " + configList[0] + " season, Week " + configList[1] + ": " + str(total) + " points!")
